535176e73063ea11d507a9912751ee91d173740f
Saisuman Dash <saisumandash0@gmail.com>
2026-06-18 23:58:11 +0530

    Time: 71 ms (23.22%), Space: 43.2 MB (43.73%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def rearrangeArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
         n = len(nums)
         ne , p =0,0
         pos = []
@@ -14,10 +10,7 @@ class Solution(object):
                 pos.append(nums[i])
             elif(nums[i]<0):
                 neg.append(nums[i])
-        for i in range (0,n/2):
+        for i in range (0,int(n/2)):
             listi.append(pos[i])
-            
             listi.append(neg[i]) 
-        return listi        
-
-        
+        return listi    
