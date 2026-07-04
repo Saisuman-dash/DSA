7cf397d56a26dff601fc5f603a702f3b418899c4
Saisuman Dash <saisumandash0@gmail.com>
2026-07-04 15:54:28 +0530

    Time: 55 ms (43.23%), Space: 22.4 MB (42.71%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -6,15 +6,29 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        freq = []
-        temp = head
-        while temp != None:
-            if temp not in freq:
-                freq.append(temp)
-                temp = temp.next
-            else:
-                return temp
+        # freq = []
+        # temp = head
+        # while temp != None:
+        #     if temp not in freq:
+        #         freq.append(temp)
+        #         temp = temp.next
+        #     else:
+        #         return temp
+        # return None
+        
+        slow=head
+        fast=head
+        while fast != None and fast.next != None :
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                slow = head
+                while slow!=fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow 
         return None
+
         
 
 
