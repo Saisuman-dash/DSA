f9408c3643b1df761d4c37c75b7b379685d96f73
Saisuman Dash <saisumandash0@gmail.com>
2026-07-08 16:53:28 +0530

    Time: 1258 ms (5.02%), Space: 19.5 MB (7.23%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,18 +1,40 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        left = 0
-        freq = set()
-        maxi = 0
-        n = len(s)
-        for right in range (0,n):
-            
-            while s[right] in freq:
-                
-                freq.remove(s[left])
-                left += 1
-            maxi = max(maxi,right-left+1)
-            freq.add(s[right])
-        return maxi
+        self.count =0
+        self.maxi=1
+        self.s=s
+        self.n=len(self.s)
+        self.lst=[]
+        if not self.n:
+            return 0
+        for i in range(0,self.n):
+            j = i
+            while j<self.n and self.s[j] not in self.lst:
+                self.lst.append(self.s[j])
+                j+=1
+            self.count = len(self.lst)
+            self.maxi = max(self.count,self.maxi)
+            self.lst.clear()
+        return self.maxi
 
-            
+        # for i in self.s:
+        #     if i in self.freq:
+        #         self.count=0
+        #         for k in self.freq:
+        #             self.freq[k]=0
+        #     self.freq[i]=self.freq.get(i,0)+1
+        #     self.count+=1
+        #     self.maxi=max(self.maxi,self.count)
+        # return self.maxi
+        # for i in range(0,self.n-1):
+        #     j = i+1
+        #     while j<self.n and self.s[j]!=self.s[i] : 
+        #         self.count= j-i+1
+        #         self.maxi=max(self.maxi,self.count)
+        #         j+=1
+        #     self.count = 0
+        # return self.maxi
+
+     
+
         
