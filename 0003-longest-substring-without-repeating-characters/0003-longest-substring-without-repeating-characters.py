0e658545abbc01cd354069f0659723f90e1c6819
Saisuman Dash <saisumandash0@gmail.com>
2026-07-09 09:31:23 +0530

    Time: 23 ms (17.95%), Space: 19.3 MB (29.42%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,40 +1,24 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        self.count =0
-        self.maxi=1
-        self.s=s
+        self.s = s
+        self.seen = {}
+        self.left=0
+        self.right=0
         self.n=len(self.s)
-        self.lst=set()
+        self.maxi=1
+        self.length=0
         if not self.n:
             return 0
-        for i in range(0,self.n):
-            j = i
-            while j<self.n and self.s[j] not in self.lst:
-                self.lst.add(self.s[j])
-                j+=1
-            self.count = len(self.lst)
-            self.maxi = max(self.count,self.maxi)
-            self.lst.clear()
+        while self.right<self.n:
+            if self.s[self.right] in self.seen and self.seen[self.s[self.right]]>=self.left:
+                self.left = self.seen[s[self.right]]+1
+                self.seen[s[self.right]]=self.right
+
+
+
+            self.maxi=max(self.maxi,self.right-self.left+1)
+            self.seen[self.s[self.right]]=self.right
+            self.right+=1
         return self.maxi
+            
 
-        # for i in self.s:
-        #     if i in self.freq:
-        #         self.count=0
-        #         for k in self.freq:
-        #             self.freq[k]=0
-        #     self.freq[i]=self.freq.get(i,0)+1
-        #     self.count+=1
-        #     self.maxi=max(self.maxi,self.count)
-        # return self.maxi
-        # for i in range(0,self.n-1):
-        #     j = i+1
-        #     while j<self.n and self.s[j]!=self.s[i] : 
-        #         self.count= j-i+1
-        #         self.maxi=max(self.maxi,self.count)
-        #         j+=1
-        #     self.count = 0
-        # return self.maxi
-
-     
-
-        
