daf2b087fc093e3c8ddb167bf2a0030cb6dc72e2
Saisuman Dash <saisumandash0@gmail.com>
2026-07-12 20:22:50 +0530

    Time: 103 ms (5.32%), Space: 23.5 MB (37.33%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -0,0 +1,26 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        income={5:0,10:0,20:0}
+        toret=True
+        for i in bills:
+            if i==5:
+                income[i]+=1
+            else:
+                # if i==5 and income[5]>=1:
+                if i==10 and income[5]>=1:
+                    income[5]-=1
+                    income[i]+=1
+                elif i==20 and (income[10]>=1 and income[5]>=1) or (income[5]>=3):
+                    if income[10]>=1 and income[5]>=1:
+                        income[10]-=1
+                        income[5]-=1
+                        income[20]+=1
+                    elif income[5]>=3:
+                        income[5]-=3
+                        income[20]+=1
+                else:
+                    toret = False
+        return toret
+            
+
+        
