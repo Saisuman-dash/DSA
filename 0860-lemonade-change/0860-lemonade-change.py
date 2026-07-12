5da63d0457c7652c0577bd5eb2fa24b6b123cbbe
Saisuman Dash <saisumandash0@gmail.com>
2026-07-12 20:24:51 +0530

    Time: 8 ms (16.29%), Space: 23.3 MB (75.73%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -1,6 +1,6 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        income={5:0,10:0,20:0}
+        income={5:0,10:0}
         toret=True
         for i in bills:
             if i==5:
@@ -14,10 +14,10 @@ class Solution:
                     if income[10]>=1 and income[5]>=1:
                         income[10]-=1
                         income[5]-=1
-                        income[20]+=1
+                        # income[20]+=1
                     elif income[5]>=3:
                         income[5]-=3
-                        income[20]+=1
+                        # income[20]+=1
                 else:
                     return False
         return toret
