03bd55ad5b5c456619f7d050ce573d740ba701a2
Saisuman Dash <saisumandash0@gmail.com>
2026-07-20 13:18:44 +0530

    Time: 0 ms (100%), Space: 19.2 MB (65.97%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
@@ -1,18 +1,51 @@
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
-        ans = []
-        
-        j = 0  # target pointer
-        
-        for i in range(1, n+1):
-            if j == len(target):
+        ans=[]
+        pt=0
+        for i in range (1,n+1):
+            if pt == len(target):
                 break
-            
-            ans.append("Push")
-            
-            if target[j] == i:
-                j += 1
+            ans.append('Push')
+            # if pt == len(target):
+            #     break
+            if target[pt]==i:
+                pt+=1
             else:
-                ans.append("Pop")
-                
+                ans.append('Pop')
         return ans
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # ans = []
+        
+        # j = 0  # target pointer
+        
+        # for i in range(1, n+1):
+        #     if j == len(target):
+        #         break
+            
+        #     ans.append("Push")
+            
+        #     if target[j] == i:
+        #         j += 1
+        #     else:
+        #         ans.append("Pop")
+                
+        # return ans
