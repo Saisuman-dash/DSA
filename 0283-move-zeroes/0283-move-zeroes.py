826085069948818e994c5bab8aca7a9f9dbdfc87
Saisuman Dash <saisumandash0@gmail.com>
2026-07-22 20:51:18 +0530

    Time: 7 ms (40.56%), Space: 20.7 MB (23.75%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,20 +3,36 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        pt=-1
-        if len(nums)==1:
-            return nums
-        for i in range(len(nums)):
-            if nums[i]==0:
-                pt=i
-                break
-        if pt==-1:
-            return nums
-        for i in range(pt,len(nums)):
-            if nums[i]!=0 :
-                nums[i],nums[pt]=nums[pt],nums[i]
+        pt=0
+        for j in range(0,len(nums)):
+            if nums[j]!=0:
+                nums[pt],nums[j]=nums[j],nums[pt]
                 pt+=1
-        return nums
+
+
+            
+            
+
+
+
+
+
+
+
+        # pt=-1
+        # if len(nums)==1:
+        #     return nums
+        # for i in range(len(nums)):
+        #     if nums[i]==0:
+        #         pt=i
+        #         break
+        # if pt==-1:
+        #     return nums
+        # for i in range(pt,len(nums)):
+        #     if nums[i]!=0 :
+        #         nums[i],nums[pt]=nums[pt],nums[i]
+        #         pt+=1
+        # return nums
 
 
       
