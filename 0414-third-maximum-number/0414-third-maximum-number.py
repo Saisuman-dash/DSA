1e5680546ee960f8eee0a935b5903ba186dc10e4
Saisuman Dash <saisumandash0@gmail.com>
2026-07-25 23:22:15 +0530

    Time: 0 ms (100%), Space: 19.8 MB (72.82%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -1,23 +1,23 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        nums=sorted(list(set(nums)))
+        # nums=sorted(list(set(nums)))
         if len(nums)<3:
             return max(nums)
-        return nums[len(nums)-3]
+        # return nums[len(nums)-3]
         
-        # max1=0
-        # max2=0
-        # max3=0
-        # for i in range (len(nums)):
-        #     if nums[i]>max1:
-        #         max3=max2
-        #         max2=max1
-        #         max1=nums[i]
-        #     if nums[i]!=max1 and nums[i]>max2:
-        #         max3=max2
-        #         max2=nums[i]
-        #     if nums[i]!=max2 and nums[i]!=max1 and nums[i]>max3:
-        #         max3=nums[i]
-        # if max3==-1:
-        #     return max(nums)
-        # return max3
+        max1=float('-inf')
+        max2=float('-inf')
+        max3=float('-inf')
+        for i in range (len(nums)):
+            if nums[i]>max1:
+                max3=max2
+                max2=max1
+                max1=nums[i]
+            if nums[i]!=max1 and nums[i]>max2:
+                max3=max2
+                max2=nums[i]
+            if nums[i]!=max2 and nums[i]!=max1 and nums[i]>max3:
+                max3=nums[i]
+        if max3==float('-inf'):
+            return max(nums)
+        return max3
