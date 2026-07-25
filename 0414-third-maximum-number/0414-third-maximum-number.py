ae4ef2d688b52026a5181a325b44d68b4eceaf0d
Saisuman Dash <saisumandash0@gmail.com>
2026-07-25 23:23:01 +0530

    Time: 0 ms (100%), Space: 19.8 MB (64.48%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -13,10 +13,10 @@ class Solution:
                 max3=max2
                 max2=max1
                 max1=nums[i]
-            if nums[i]!=max1 and nums[i]>max2:
+            elif nums[i]!=max1 and nums[i]>max2:
                 max3=max2
                 max2=nums[i]
-            if nums[i]!=max2 and nums[i]!=max1 and nums[i]>max3:
+            elif nums[i]!=max2 and nums[i]!=max1 and nums[i]>max3:
                 max3=nums[i]
         if max3==float('-inf'):
             return max(nums)
