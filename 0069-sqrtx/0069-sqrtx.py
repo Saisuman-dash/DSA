cc5d5b85b9117d90dc9aeaf1074abbcf87cc742b
Saisuman Dash <saisumandash0@gmail.com>
2026-07-26 23:14:06 +0530

    Time: 2 ms (73.35%), Space: 19.2 MB (57.5%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x<=1:
+            return x
+        low=0
+        high=x//2
+        while low<=high:
+            mid=(low+high)//2
+            if mid*mid==x:
+                return mid
+            elif mid*mid<x:
+                low=mid+1
+            elif mid*mid>x:
+                high=mid-1
+        return high
