2821ffb3da1874a1216ec4d2a48ccdf45a8de1b3
Saisuman Dash <saisumandash0@gmail.com>
2026-07-26 23:40:47 +0530

    Time: 2 ms (9.16%), Space: 20.5 MB (58.85%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,21 +1,61 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        # found=False
+        low =0
+        high=len(nums)-1
         first=-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                first=mid
+                high=mid-1
+            elif nums[mid]<target:
+                low=mid+1
+            elif nums[mid]>target:
+                high=mid-1
+        low =0
+        high=len(nums)-1
         last=-1
-        for i in range (len(nums)):
-            if nums[i]==target and first==-1:
-                first=i
-                # found=True
-            if nums[i]==target and i!=first:
-                last=i
-                # found=True
-        # if found==False:
-        #     return [-1,-1]
-        if first!=-1 and last!=-1:
-            return [first,last]
-        elif first==-1:
-            return [first,last]
-        elif first!=-1 and last==-1:
-            return [first,first]
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                last=mid
+                low=mid+1
+            elif nums[mid]<target:
+                low=mid+1
+            elif nums[mid]>target:
+                high=mid-1
+        return [first,last]
+        
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # first=-1
+        # last=-1
+        # for i in range (len(nums)):
+        #     if nums[i]==target and first==-1:
+        #         first=i
+            
+        #     if nums[i]==target and i!=first:
+        #         last=i
+           
+        # if first!=-1 and last!=-1:
+        #     return [first,last]
+        # elif first==-1:
+        #     return [first,last]
+        # elif first!=-1 and last==-1:
+        #     return [first,first]
         
