4ec16a5a71ee49da10b1f83a0b7819e1881a08c0
Saisuman Dash <saisumandash0@gmail.com>
2026-07-28 17:12:13 +0530

    Time: 0 ms (100%), Space: 19.3 MB (60.73%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,18 +1,21 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        low = 0
-        mid = 0
-        high = len(nums)-1
-        
-        while mid <= high:
-            if nums[mid] == 0:
-                nums[low], nums[mid] = nums[mid], nums[low]
-                low += 1
-                mid += 1
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left=0
+        mid=0
+        right=len(nums)-1
+        while mid<=right:
+            if nums[mid]==0:
+                nums[left],nums[mid]=nums[mid],nums[left]
+                left+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
+            else :
+                nums[right],nums[mid]=nums[mid],nums[right]
+                right-=1
                 
-            elif nums[mid] == 1:
-                mid += 1
-                
-            else: 
-                nums[mid], nums[high] = nums[high], nums[mid]
-                high -= 1
+            
+
