391ed0c84d1cdee64b2cfb3e58c760322bd254e2
Saisuman Dash <saisumandash0@gmail.com>
2026-07-28 16:50:18 +0530

    Time: 7 ms (73.65%), Space: 20.9 MB (83.71%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -2,21 +2,42 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         
         total=0
-        leftmaxi=[0]*len(height)
-        rightmaxi=[0]*len(height)
+        left=0
+        right=len(height)-1
         leftmax=float('-inf')
         rightmax=float('-inf')
 
-        for i in range (len(leftmaxi)):
-            if height[i]>leftmax:
-                leftmax=height[i]
-            leftmaxi[i]=leftmax
-        for j in range (len(rightmaxi)-1,-1,-1):
-            if height[j]>rightmax:
-                rightmax=height[j]
-            rightmaxi[j]=rightmax
-        for i in range (len(height)):
-            total+=min(leftmaxi[i],rightmaxi[i])-height[i]
-        
+        while left<right:
+            if height[left]<=height[right]:
+                leftmax=max(leftmax,height[left])
+                total+=leftmax-height[left]
+                left+=1
+            elif height[left]>height[right]:
+                rightmax=max(rightmax,height[right])
+                total+=rightmax-height[right]
+                right-=1
         return total
+
+
+
+
+
+        # for i in range (len(height)):
+            
+        #     total+=min(leftmaxi[i],rightmaxi[i])-height[i]
+        
+        # return total
+
+
+            # for i in range (len(leftmaxi)):
+        #     if height[i]>leftmax:
+        #         leftmax=height[i]
+        #     leftmaxi[i]=leftmax
+        # print(leftmaxi)
+        # for j in range (len(rightmaxi)-1,-1,-1):
+        #     if height[j]>rightmax:
+        #         rightmax=height[j]
+        #     rightmaxi[j]=rightmax
+        # print(rightmaxi)
+
        
