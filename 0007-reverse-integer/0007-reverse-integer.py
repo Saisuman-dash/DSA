6bcc277b4fd30a72c331112250e73f1c2dc14b6c
Saisuman Dash <saisumandash0@gmail.com>
2026-07-30 21:13:40 +0530

    Time: 51 ms (25.13%), Space: 19.2 MB (69.36%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -7,10 +7,11 @@ class Solution:
         while p//1!=0:
             val=p%10
             ans=ans*10+val
+            if ans<=(-1*2**31) or ans>=(2**31)-1:
+                return 0
             tenp*=10
             p=p//10
         if x<0:
             ans= -1*ans
-        if ans<=(-1*2**31) or ans>=(2**31)-1:
-            return 0
+        
         return ans
