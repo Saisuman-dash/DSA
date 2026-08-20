14189320ca409ce885be31552413b7d391926dd4
Saisuman Dash <saisumandash0@gmail.com>
2026-08-21 00:22:26 +0530

    Time: 0 ms (100%), Space: 12.4 MB (19.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,20 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        hash={}
-        ans=0
-        def climb(n):
-            if n==1 or n==2:
-                return n
-            if n in hash:
-                return hash[n]
-            else:
-                ans = climb(n-1)+climb(n-2)
-                hash[n]=ans
-                return ans
-        return climb(n)
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        def ppt(n,qb):
+            if n<0:
+                return 0
+            if n==0:
+                return 1
+            if n in qb:
+                return qb[n]
+            bugu=ppt(n-1,qb)+ppt(n-2,qb)
+            qb[n]=bugu
+            return bugu
+        qb={}
+        ansu=ppt(n,qb)
+        return ansu
+        
